0c9003ad8d3284592aa089b9d24c2db4384ff88b
lulzshadowwalker <lulzshadowwalker@gmail.com>
2026-08-28 14:11:12 +0300

    Add map and mapb

--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/lulzshadowwalker/pokedex/repl"
 )
 
 func main() {
-	registry := registry{commands: make(map[string]command)}
+	registry := registry{
+		config: &config{
+			next: new("https://pokeapi.co/api/v2/location-area"),
+		},
+		commands: make(map[string]command),
+	}
 	commands := []command{
 		{
 			name:        "exit",
 			description: "Exit the Pokdex",
-			callback: func() error {
+			callback: func(config *config) error {
 				fmt.Printf("Closing the Pokedex... Goodbye!")
 				os.Exit(0)
 				return nil
@@ -25,10 +32,92 @@ func main() {
 		{
 			name:        "help",
 			description: "Displays a help message",
-			callback: func() error {
+			callback: func(config *config) error {
 				fmt.Printf("Welcome to the Pokedex!\nUsage:\n\n")
 				for _, command := range registry.commands {
 					fmt.Printf("%s: %s\n", command.name, command.description)
+				}
+
+				return nil
+			},
+		},
+
+		{
+			name:        "map",
+			description: "Displays a paginated list of areas in the Pokemon world",
+			callback: func(config *config) error {
+				if config.next == nil {
+					return nil
+				}
+
+				response, err := http.Get(*config.next)
+				if err != nil {
+					return err
+				}
+				defer response.Body.Close()
+
+				if response.StatusCode != http.StatusOK {
+					return ErrUnknown
+				}
+
+				decoder := json.NewDecoder(response.Body)
+				data := struct {
+					Previous *string
+					Next *string
+					Results []struct{
+						Name string
+					}
+				}{}
+				if err := decoder.Decode(&data); err != nil {
+					return err
+				}
+
+				config.previous = data.Previous
+				config.next = data.Next
+
+				for _, result := range data.Results {
+					fmt.Println(result.Name)
+				}
+
+				return nil
+			},
+		},
+
+		{
+			name:        "mapb",
+			description: "Displays a paginated list of areas in the Pokemon world (map backward)",
+			callback: func(config *config) error {
+				if config.previous == nil {
+					return nil
+				}
+
+				response, err := http.Get(*config.previous)
+				if err != nil {
+					return err
+				}
+				defer response.Body.Close()
+
+				if response.StatusCode != http.StatusOK {
+					return ErrUnknown
+				}
+
+				decoder := json.NewDecoder(response.Body)
+				data := struct {
+					Previous *string
+					Next *string
+					Results []struct{
+						Name string
+					}
+				}{}
+				if err := decoder.Decode(&data); err != nil {
+					return err
+				}
+
+				config.previous = data.Previous
+				config.next = data.Next
+
+				for _, result := range data.Results {
+					fmt.Println(result.Name)
 				}
 
 				return nil
@@ -62,17 +151,24 @@ func main() {
 	}
 }
 
+type config struct {
+	next     *string
+	previous *string
+}
+
 type command struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(config *config) error
 }
 
 type registry struct {
+	config   *config
 	commands map[string]command
 }
 
 var ErrCommandUnknown = errors.New("Unknown command")
+var ErrUnknown = errors.New("Unknown error has occurred")
 
 func (r *registry) run(name string) error {
 	command, ok := r.commands[name]
@@ -80,5 +176,5 @@ func (r *registry) run(name string) error {
 		return ErrCommandUnknown
 	}
 
-	return command.callback()
+	return command.callback(r.config)
 }
